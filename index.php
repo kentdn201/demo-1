8aa16f294f51e9dcdb1e48ada758711c014d251a
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-01 15:35:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$username = $_POST['username'];
  	$password = $_POST['password'];
- 	$sql="SELECT * FROM account WHERE user_name ='$username' and pass='$password'";
+ 	$sql="SELECT * FROM account WHERE account ='$username' and pass='$password'";
 	$result = pg_query($conn, $sql);
 if (!$result) {
   echo "An error occurred.\n";
